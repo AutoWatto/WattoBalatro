d1b7726d10ad082eda288d76188b677ebb662f01
oklexi <hiimlexilol@gmail.com>
2025-03-16 01:24:25 -0600

    added oops deck loc and added sleeve

--- Oopsall6s.lua
+++ Oopsall6s.lua
@@ -2,19 +2,33 @@ local current_mod = SMODS.current_mod
 local mod_path = SMODS.current_mod.path
 
 SMODS.Back{
-    key = "oops_all_6s",
-    atlas = 'oopsall6sdeck', 
-    pos = { x = 0, y = 0 }, 
-    config = { joker_slot = 5 },
+    key = "oopsall6s",
+    atlas = 'oopsall6sdeck',
+    pos = { x = 0, y = 0 },
     apply = function(self)
         G.E_MANAGER:add_event(Event({
             func = function()
-               G.GAME.probabilities.normal = G.GAME.probabilities.normal * 2
+                G.GAME.probabilities.normal = G.GAME.probabilities.normal * 2
                 return true
             end,
         }))
     end,
 }
+if CardSleeves then
+    CardSleeves.Sleeve {
+        key = "oopsall6s",
+        atlas = 'oopsall6sdeck',
+        pos = { x = 1, y = 0 },
+        apply = function(self)
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    G.GAME.probabilities.normal = G.GAME.probabilities.normal * 2
+                    return true
+                end,
+            }))
+        end,
+    }
+end
 
 SMODS.Atlas({
 	key = "oopsall6sdeck",
